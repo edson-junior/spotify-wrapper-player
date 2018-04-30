cab3edd65a65934a8c80a8410b7bca99678f732a
Edson Junior <edson-junior@g-star.com>
2018-04-30 17:17:32 +0200

    adicionando o albumlist no layout

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,8 @@
+
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: 'YOUR_TOKEN_HERE',
+  token: 'BQDya6IzNsh5d32d8jzblGrJ81Ca_2LoX-XZcTzw6t9GrCFieRo8BQbrG0bgdyQ94tHpOSuP0DHEOSf_kTA3Y1QAaXFUgk4I5aruHohJ64ZlcGLqNidSTK8G5wA2_HncNKFvnXndDUuRbbw3',
 });
 
 export default spotify;
