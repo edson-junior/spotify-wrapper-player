c04faf910ec15aba43c965c1ccc810a6dacf8472
Edson Junior <soneddjr@gmail.com>
2018-05-01 19:00:20 +0200

    fix image in album info

--- src/AlbumInfo.js
+++ src/AlbumInfo.js
@@ -1,7 +1,7 @@
 export default function renderAlbumdata(data, element) {
   const elem = element;
   const markup = `
-    <img class="album-image" src="${data.images[2].url}" alt="${data.artists[0].name}">
+    <img class="album-image" src="${data.images[0].url}" alt="${data.artists[0].name}">
     <p class="album-title">${data.name}</p>
     <p class="album-artist">${data.artists[0].name}</p>
     <p class="album-counter">${data.tracks.total} Músicas</p>`;
