3371ffbbcd8c9d4ca36369e7a5cca80e7cb6d34a
Edson Junior <soneddjr@gmail.com>
2018-05-01 19:59:27 +0200

    handleling testing with mocha

--- src/Spotify.js
+++ src/Spotify.js
@@ -4,6 +4,10 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
+  // unfortunatelly because of the way the spotify-wrapper plugin
+  // was developed, it is necessary to comment/uncomment this fake token
+  // in order to run the tests
+  // token: 'BQD1emHdgnpHskVIGfhSbQqkXTUZmHXOERgQAXVeM4yKMzv7tDh8J-GpxDFwSp2krQE1EtZOVnUEMxPobRg',
   token: window.AppSettings.token,
 });
 
