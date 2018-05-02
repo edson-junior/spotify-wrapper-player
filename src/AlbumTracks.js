e78ade8e224809c71ba64662e73ac945b5ac5186
Edson Junior <soneddjr@gmail.com>
2018-05-02 18:15:27 +0200

    criando componente albumtracks

---
+++ src/AlbumTracks.js
@@ -0,0 +1,14 @@
+function createMarkup(tracks) {
+  return tracks.map(track => `
+    <div class="music" data-track-preview="${track.preview_url}">
+      <p class="music-number">${track.track_number}</p>
+      <p class="music-title">${track.name}</p>
+      <p class="music-duration">${track.duration_ms}</p>
+    </div>`).join('');
+}
+
+export default function renderAlbumTracks(data, element) {
+  const markup = createMarkup(data);
+  const elem = element;
+  elem.innerHTML = markup;
+}
