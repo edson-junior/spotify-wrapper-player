2d82bbfcc6f623fc65a7c858c1cf6fbff384db56
Edson Junior <soneddjr@gmail.com>
2018-05-03 17:06:39 +0200

    criando searchtrigger

---
+++ src/SearchTrigger.js
@@ -0,0 +1,16 @@
+/* eslint-env browser */
+
+import spotify from './Spotify';
+import renderAlbums from './AlbumList';
+
+const albumList = document.getElementById('album-list');
+const searchInput = document.getElementById('search-input');
+const searchForm = document.getElementById('search-form');
+
+export default function searchEnterTrigger() {
+  searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    spotify.search.albums(searchInput.value)
+      .then(data => renderAlbums(data.albums.items, albumList));
+  })
+}
