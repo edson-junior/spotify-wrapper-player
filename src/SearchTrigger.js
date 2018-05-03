92b3e71f7bc8ca92790d7befe611e09a3effc05f
Edson Junior <soneddjr@gmail.com>
2018-05-03 17:08:58 +0200

    eslint fixes

--- src/SearchTrigger.js
+++ src/SearchTrigger.js
@@ -8,9 +8,9 @@ const searchInput = document.getElementById('search-input');
 const searchForm = document.getElementById('search-form');
 
 export default function searchEnterTrigger() {
-  searchForm.addEventListener('submit', e => {
+  searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     spotify.search.albums(searchInput.value)
       .then(data => renderAlbums(data.albums.items, albumList));
-  })
+  });
 }
